7eeede0d848ad04e59bc23fd7d22be22c9ebe140
AkeinR <105894263+AkeinR@users.noreply.github.com>
2024-08-18 14:16:54 +1000

    Arrow key based camera panning
    y axis isn't working but x axis is fine

--- Scripts/Camera2D.gd
+++ Scripts/Camera2D.gd
@@ -1,5 +1,8 @@
 extends Camera2D
 
+
+@export var panSpeed = 1000
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -7,6 +10,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var mVel = Input.get_last_mouse_velocity()
-	if Input.is_action_pressed("pan"):
-		position = (mVel * delta) * 16
+	var dir = Input.get_vector("left", "right", "up", "down")
+	print(position + ((dir * panSpeed) * delta))
+	position += ((dir * panSpeed) * delta)
+	print(position)
